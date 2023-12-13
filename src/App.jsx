93f9da4d75553534c78e7177a5390e60a0c88b6e
Ra <ranilsd86@gmail.com>
2023-12-13 13:18:42 -0500

    added more future project pics and imported almost all images

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Projects from "./pages/Projects";
+import TechStack from "./pages/TechStack";
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/techStack" element={<TechStack />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+export default App;
---
+++ src/Details.jsx
@@ -0,0 +1,26 @@
+//importing my profile image
+import profile from "./assets/profile.jpg";
+
+//importing the tekdek pictures
+import bootstrap from "./assets/tekdek/bootstrap.png";
+import css from "./assets/tekdek/css.png";
+import expressjs from "./assets/tekdek/expressjs.png";
+import git from "./assets/tekdek/git.png";
+import github from "./assets/tekdek/github.png";
+import html from "./assets/tekdek/html.png";
+import javascript from "./assets/tekdek/javascript.png";
+import mongodb from "./assets/tekdek/mongodb.png";
+import mysql from "./assets/tekdek/mysql.png";
+import nodejs from "./assets/tekdek/nodejs.png";
+import npm from "./assets/tekdek/npm.png";
+import react from "./assets/tekdek/react.png";
+import tailwind from "./assets/tekdek/tailwind.png";
+import vscode from "./assets/tekdek/vscode.png";
+
+
+//importing my project pictures
+
+import projectPic1 from "./assets/projects/project1.jpg";
+import projectPic2 from "./assets/projects/project2.jpg";
+import projectPic3 from "./assets/projects/project3.jpg";
+import projectPic4 from "./assets/projects/project4.jpg";
